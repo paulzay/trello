72c6338b363d1ac6626f7bdf51156f1bbba35f0a
paulzay <paulotieno2@gmail.com>
2021-02-08 21:00:56 +0300

    implement adding lists reducer

--- src/AppStateContext.tsx
+++ src/AppStateContext.tsx
@@ -1,7 +1,9 @@
 import React, { createContext, useReducer, useContext } from "react";
+import { nanoid } from "nanoid";
 
 interface AppStateContextProps {
     state: AppState
+    dispatch: React.Dispatch<Action>
 }
 const AppStateContext = createContext<AppStateContextProps>({} as AppStateContextProps)
 
@@ -38,12 +40,46 @@ lists: List[]
 }
 
 export const AppStateProvider = ({ children }: React.PropsWithChildren<{}>) => {
+    const [state, dispatch] = useReducer(appStateReducer, appData)
     return (
-    <AppStateContext.Provider value={{ state: appData }}>
+    <AppStateContext.Provider value={{ state, dispatch}}>
     {children}
     </AppStateContext.Provider>
     )
 }
 export const useAppState = () => {
     return useContext(AppStateContext)
+}
+
+type Action =
+    | {
+    type: "ADD_LIST"
+    payload: string
+    }
+    | {
+    type: "ADD_TASK"
+    payload: { text: string; listId: string }
+}
+
+const appStateReducer = (state: AppState, action: Action): AppState => {
+    switch(action.type){
+        case "ADD_LIST":{
+            return {
+                ...state,
+                lists: [
+                    ...state.lists,
+                    {id: nanoid(), text: action.payload, tasks: []}
+                ]
+            }
+        }
+        case "ADD_TASK":{
+            return {
+                ...state
+            }
+        }
+        default:{
+            return state
+        }
+
+    }
 }
