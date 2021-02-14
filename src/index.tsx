5d1dbeda1957b549e296d7068c6a0b99eb901e0d
paulzay <paulotieno2@gmail.com>
2021-02-14 20:23:24 +0300

    add dnd provider to index

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { AppStateProvider } from "./AppStateContext";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend as Backend} from 'react-dnd-html5-backend';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <DndProvider backend={Backend}>
     <AppStateProvider>
       <App />
     </AppStateProvider>
-  </React.StrictMode>,
+  </DndProvider>,
   document.getElementById('root')
 );
