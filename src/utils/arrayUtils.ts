87992b6f4a5d63ce8dfb8e4e31c6b9d2bad49482
paulzay <paulotieno2@gmail.com>
2021-02-15 21:27:47 +0300

    remove the item from it’s original position

--- src/utils/arrayUtils.ts
+++ src/utils/arrayUtils.ts
@@ -17,3 +17,10 @@ export function overrideItemAtIndex<T>(
     return newItem
     })
 }
+export function removeItemAtIndex<T>(array: T[], index: number) {
+    return [...array.slice(0, index), ...array.slice(index + 1)]
+}
+export const moveItem = <T>(array: T[], from: number, to: number) => {
+    const item = array[from]
+    return insertItemAtIndex(removeItemAtIndex(array, from), item, to)
+}
